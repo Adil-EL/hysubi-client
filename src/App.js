2665a9dbc25b208462848b985f0da84a4fa0d407
anas-r <anas-r@github.io>
2022-06-12 12:31:47 +0200

    small edit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Sango Technologies corporation</h1>
-      <h3> Hysubi platform</h3>
-      <p> Coming soon ...</p>
+      <h1>Sango Technologies corporation</h1>
+      <h3>Hysubi platform</h3>
+      <p>Our platform is coming soon</p>
     </div>
   );
 }
