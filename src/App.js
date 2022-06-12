01353e6fd6ecbbde96576f4ac2b6c9da1e27fc3c
Adil-EL <adilelhakouni.lt@gmail.com>
2022-06-12 12:20:02 +0200

    Adjust the name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> SangoTechnologies corporation</h1>
+      <h1> Sango Technologies corporation</h1>
       <h3> Hysubi platform</h3>
       <p> Coming soon ...</p>
     </div>
